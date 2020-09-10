25e664bc81a63a4460bd72436b1b1fd683ee1473
ClaytonC <cjcanady1113@gmail.com>
2020-09-10 12:11:22 -0400

    user auth working

--- meat_master_api/views.py
+++ meat_master_api/views.py
@@ -2,17 +2,15 @@ from rest_framework import generics
 from .serializers import RecipeSerializer
 from .models import Recipe
 
+
 # Create your views here.
 
 
 class RecipeList(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-
+    
 
 class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-
-
-
